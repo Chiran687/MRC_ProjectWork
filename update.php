974e569f6f11f5d9c6c38d9869cb15d3ea5189e8
Chiran687 <chiranjung@outlook.com>
2023-12-27 11:48:32 +0545

    2023-12-26

---
+++ update.php
@@ -0,0 +1,171 @@
+<?php
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$name = $address = $roll_no = "";
+$name_err = $address_err = $roll_no_err = "";
+ 
+// Processing form data when form is submitted
+if(isset($_POST["id"]) && !empty($_POST["id"])){
+    // Get hidden input value
+    $id = $_POST["id"];
+    
+    // Validate name
+    $input_name = trim($_POST["name"]);
+    if(empty($input_name)){
+        $name_err = "Please enter a name.";
+    } elseif(!filter_var($input_name, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+        $name_err = "Please enter a valid name.";
+    } else{
+        $name = $input_name;
+    }
+    
+    // Validate address address
+    $input_address = trim($_POST["address"]);
+    if(empty($input_address)){
+        $address_err = "Please enter an address.";     
+    } else{
+        $address = $input_address;
+    }
+    
+    // Validate roll_no
+    $input_roll_no = trim($_POST["roll_no"]);
+    if(empty($input_roll_no)){
+        $roll_no_err = "Please enter the roll_no amount.";     
+    } elseif(!ctype_digit($input_roll_no)){
+        $roll_no_err = "Please enter a positive integer value.";
+    } else{
+        $roll_no = $input_roll_no;
+    }
+    
+    // Check input errors before inserting in database
+    if(empty($name_err) && empty($address_err) && empty($roll_no_err)){
+        // Prepare an update statement
+        $sql = "UPDATE students SET name=:name, address=:address, roll_no=:roll_no WHERE id=:id";
+ 
+        if($stmt = $pdo->prepare($sql)){
+            // Bind variables to the prepared statement as parameters
+            $stmt->bindParam(":name", $param_name);
+            $stmt->bindParam(":address", $param_address);
+            $stmt->bindParam(":roll_no", $param_roll_no);
+            $stmt->bindParam(":id", $param_id);
+            
+            // Set parameters
+            $param_name = $name;
+            $param_address = $address;
+            $param_roll_no = $roll_no;
+            $param_id = $id;
+            
+            // Attempt to execute the prepared statement
+            if($stmt->execute()){
+                // Records updated successfully. Redirect to landing page
+                header("location: index.php");
+                exit();
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+         
+        // Close statement
+        unset($stmt);
+    }
+    
+    // Close connection
+    unset($pdo);
+} else{
+    // Check existence of id parameter before processing further
+    if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
+        // Get URL parameter
+        $id =  trim($_GET["id"]);
+        
+        // Prepare a select statement
+        $sql = "SELECT * FROM students WHERE id = :id";
+        if($stmt = $pdo->prepare($sql)){
+            // Bind variables to the prepared statement as parameters
+            $stmt->bindParam(":id", $param_id);
+            
+            // Set parameters
+            $param_id = $id;
+            
+            // Attempt to execute the prepared statement
+            if($stmt->execute()){
+                if($stmt->rowCount() == 1){
+                    /* Fetch result row as an associative array. Since the result set
+                    contains only one row, we don't need to use while loop */
+                    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+                
+                    // Retrieve individual field value
+                    $name = $row["name"];
+                    $address = $row["address"];
+                    $roll_no = $row["roll_no"];
+                } else{
+                    // URL doesn't contain valid id. Redirect to error page
+                    header("location: error.php");
+                    exit();
+                }
+                
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+        
+        // Close statement
+        unset($stmt);
+        
+        // Close connection
+        unset($pdo);
+    }  else{
+        // URL doesn't contain id parameter. Redirect to error page
+        header("location: error.php");
+        exit();
+    }
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Update Record</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        .wrapper{
+            width: 600px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="mt-5">Update Record</h2>
+                    <p>Please edit the input values and submit to update the Student record.</p>
+                    <form action="<?php echo htmlspecialchars(basename($_SERVER['REQUEST_URI'])); ?>" method="post">
+                        <div class="form-group">
+                            <label>Name</label>
+                            <input type="text" name="name" class="form-control <?php echo (!empty($name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $name; ?>">
+                            <span class="invalid-feedback"><?php echo $name_err;?></span>
+                        </div>
+                        <div class="form-group">
+                            <label>Address</label>
+                            <textarea name="address" class="form-control <?php echo (!empty($address_err)) ? 'is-invalid' : ''; ?>"><?php echo $address; ?></textarea>
+                            <span class="invalid-feedback"><?php echo $address_err;?></span>
+                        </div>
+                        <div class="form-group">
+                            <label>roll_no</label>
+                            <input type="text" name="roll_no" class="form-control <?php echo (!empty($roll_no_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $roll_no; ?>">
+                            <span class="invalid-feedback"><?php echo $roll_no_err;?></span>
+                        </div>
+                        <input type="hidden" name="id" value="<?php echo $id; ?>"/>
+                        <input type="submit" class="btn btn-primary" value="Submit">
+                        <a href="index.php" class="btn btn-secondary ml-2">Cancel</a>
+                    </form>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
